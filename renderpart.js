ca11e8989ac9b761174decc54999fba0eb2762a2
AninditaBanerjee <73800440+AninditaBanerjee@users.noreply.github.com>
2021-01-04 04:17:03 +0530

    Add files via upload

---
+++ renderpart.js
@@ -0,0 +1,9 @@
+var render = Render.create({
+    element: document.body,
+    engine: engine,
+    options: {
+      width: 1200,
+      height: 700,
+      wireframes: false
+    }
+  });
---
+++ sketch.js
@@ -0,0 +1,55 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var dustbin , paper ,ground ;	
+var world;
+
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	dustbin =new Dustbin(1200,650);
+	paper =new Paper(200,450);
+	ground =new Ground(width/2,670,width,20);
+	 
+	
+
+	
+	Engine.run(engine);
+	//Render.run(render);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(255);
+ 
+  dustbin.display();
+  paper.display();
+  ground.display();
+  
+ 
+  
+  
+ 
+}
+
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+
+    	Matter.Body.applyForce(paper.body,paper.body.position,{x:50,y:-55});
+    
+  	}
+}
+
+
+
+
+
